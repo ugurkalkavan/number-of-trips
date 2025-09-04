e003ff9634f14c4006f044e00383c403bdd69b69
ugurkalkavan <ugur_kalkavan@epam.com>
2025-09-05 01:03:05 +0300

    query fix

--- sql/query2.sql
+++ sql/query2.sql
@@ -1,20 +1,32 @@
-WITH driver_summary AS (
+WITH done_trips_in_different_days AS (
     SELECT
-        'DRIVER' AS view_type,
-        driver_id AS id,
-        COUNT(*) AS number_of_trips
-    FROM trips
-    GROUP BY driver_id
+        client_id,
+        driver_id,
+        trip_date::date,
+        count(*) as cnt
+    FROM
+        trips
+    WHERE status = 'done'
+    GROUP BY client_id, driver_id, trip_date::date
+    HAVING count(*) < 2
 ),
 client_summary AS (
     SELECT
         'CLIENT' AS view_type,
         client_id AS id,
-        COUNT(*) AS number_of_trips
-    FROM trips
+        SUM(cnt) as total_cnt
+    FROM done_trips_in_different_days
     GROUP BY client_id
+),
+driver_summary AS (
+    SELECT
+        'DRIVER' AS view_type,
+        driver_id AS id,
+        SUM(cnt) as total_cnt
+    FROM done_trips_in_different_days
+    GROUP BY driver_id
 )
-SELECT * FROM driver_summary
-UNION
 SELECT * FROM client_summary
+UNION
+SELECT * FROM driver_summary
 ORDER BY view_type, id;
