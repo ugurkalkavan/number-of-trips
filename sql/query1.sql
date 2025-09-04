b0f61958d10da4ae2702e2bad2c01ea6a9458408
ugurkalkavan <ugur_kalkavan@epam.com>
2025-09-05 00:01:01 +0300

    query fix

--- sql/query1.sql
+++ sql/query1.sql
@@ -1,10 +1,19 @@
+WITH driver_trip_days AS (
+    SELECT
+        driver_id,
+        trip_date::date AS trip_day,
+        status,
+        COUNT(*) AS cnt
+    FROM trips
+    GROUP BY driver_id, trip_date::date, status
+)
 SELECT
     driver_id,
-    COUNT(*) FILTER (WHERE status IN ('done', 'not_respond')) AS total_days,
+    COUNT(DISTINCT trip_day) FILTER (WHERE status IN ('done', 'not_respond')) AS total_days,
     ROUND(
-        100.0 * COUNT(*) FILTER (WHERE status = 'done')
-        / COUNT(*) FILTER (WHERE status IN ('done','not_respond'))
-    , 0) AS success_rate
-FROM trips
+        100.0 * SUM(cnt) FILTER (WHERE status = 'done')
+        / SUM( cnt) FILTER (WHERE status IN ('done','not_respond'))
+    , 2) AS success_rate
+FROM driver_trip_days
 GROUP BY driver_id
 ORDER BY driver_id;
